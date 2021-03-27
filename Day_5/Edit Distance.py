e9d523867b5fae1b206afc6ee54e6f075b52bf8b
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-03-28 03:41:09 +0530

    Add files via upload

---
+++ Day_5/Edit Distance.py
@@ -0,0 +1,14 @@
+# Edit Distance
+def editDistance(str1, str2, m, n):
+    if m==0:
+        return n
+    if n==0:
+        return m
+    if str1[m-1] == str2[n-1]:
+        return editDistance(str1, str2, m-1, n-1)
+    return 1 + min(editDistance(str1, str2, m, n-1),
+                  editDistance(str1, str2, m-1, n),
+                  editDistance(str1, str2, m-1, n-1))
+str1 = 'Sunday'
+str2 = 'Saturday'
+editDistance(str1, str2, len(str1), len(str2))
