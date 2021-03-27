70f42f7dbdaee0c877f395051f5f92a79b8f222e
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-03-28 03:38:34 +0530

    Add files via upload

---
+++ Day_5/Balanced_Bracket.py
@@ -0,0 +1,24 @@
+# Balanced Bracket
+def areBracketBalanced(expr):
+    stack = []
+    for char in expr:
+        if char in ["(", "{", "["]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ')':
+                    return False
+            if current_char == '{':
+                if char != '}':
+                    return False
+            if current_char == '[':
+                if char != ']':
+                    return False
+    if stack:
+        return False
+    return True
+expr = "{()}[]"
+areBracketBalanced(expr)
