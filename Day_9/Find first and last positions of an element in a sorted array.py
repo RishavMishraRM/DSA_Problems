627c8d5ddab7e79ccf9d6af577e8b6daff0793c9
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-04-03 23:20:28 +0530

    Add files via upload

---
+++ Day_9/Find first and last positions of an element in a sorted array.py
@@ -0,0 +1,19 @@
+#Find first and last positions of an element in a sorted array
+def findFirstAndLast(arr, n, x):
+    first = -1
+    last = -1
+    for i in range(0, n):
+        if (x!=arr[i]):
+            continue
+        if (first == -1):
+            first = i
+        last = i
+    if(first!=-1):
+        print("First Occurrence = ", first,
+             "\n Last Occurrence ", last)
+    else:
+        print("Not Found")
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+n = len(arr)
+x = 8
+findFirstAndLast(arr, n, x)
