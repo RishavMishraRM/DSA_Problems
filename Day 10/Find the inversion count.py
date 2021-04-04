45090ca7a20b4562db32c2081a326cb25b935d37
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-04-05 02:22:05 +0530

    Add files via upload

---
+++ Day 10/Find the inversion count.py
@@ -0,0 +1,11 @@
+#Find the inversion count
+def getInvCount(arr, n):
+    inv_count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if(arr[i] >arr[j]):
+                inv_count += 1
+    return inv_count
+arr = [1, 20, 6, 4, 5]
+n = len(arr)
+getInvCount(arr, n)
