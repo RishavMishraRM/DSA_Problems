700b6f6c3ecd392e8c8ee99b20baecaa69547db1
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-04-06 01:07:30 +0530

    Add files via upload

---
+++ Day 11/subarrays with 0 sum.py
@@ -0,0 +1,13 @@
+#subarrays with 0 sum
+def subArray(arr, n):
+    n_sum = 0
+    s = set()
+    for i in range(n):
+        n_sum += arr[i]
+        if n_sum == 0 or n_sum in s:
+             return True
+        s.add(n_sum)
+    return False
+arr = [4, 2, 0, 1, 6]
+n = len(arr)
+subArray(arr, n)
